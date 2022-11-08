408673f8d1af040b609e8f0559727f4eeb469496
FISHcaptain <1843584822@qq.com>
2022-11-08 10:33:22 +0800

    style:输出行为一行

---
+++ c++work1-1anotherform .cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Enter two numbers:" << std::endl;
+	int a = 0, b = 0;
+	std::cin >> a >> b;
+	std::cout << "The sum of" << a << "multi" << b << "is" << a * b << std::endl;
+	return 0;
+
+}
