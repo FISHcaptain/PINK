f2f4ff79f991a1609bc65309980b631a34af6859
ulinzeng <ulinzeng@qq.com>
2022-11-29 23:29:02 +0800

    bugfix: 提交更改建议

--- 实验报告/二维数组1.cpp
+++ 实验报告/二维数组1.cpp
@@ -1,32 +1,32 @@
-//二维数组的转置
-#include<stdio.h>
-void fun(int arr[3][3]) {
-	int i, j, t = 0;
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			if (i <= j) {
-				t = arr[i][j];
-				arr[i][j] = arr[j][i];
-				arr[j][i] = t;
-			}
-		}
-	}
-}
-int main()
-{
-	int arr[3][3], i, j;
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			scanf_s("%d", &arr[i][j]);
-		}
-	}
-	fun (arr);
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-	return 0;
-}
-	
+////二维数组的转置
+//#include<stdio.h>
+//void fun(int arr[3][3]) {
+//	int i, j, t = 0;
+//	for (i = 0; i < 3; i++) {
+//		for (j = 0; j < 3; j++) {
+//			if (i <= j) {
+//				t = arr[i][j];
+//				arr[i][j] = arr[j][i];
+//				arr[j][i] = t;
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[3][3], i, j;
+//	for (i = 0; i < 3; i++) {
+//		for (j = 0; j < 3; j++) {
+//			scanf_s("%d", &arr[i][j]);
+//		}
+//	}
+//	fun (arr);
+//	for (i = 0; i < 3; i++) {
+//		for (j = 0; j < 3; j++) {
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//	
